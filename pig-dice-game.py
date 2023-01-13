0ca75f4ebda0ad76f7bfb65aaf30260863df322f
HYEON-CODE <gus7264@gmail.com>
2023-01-13 14:10:30 +0900

    feat: Create ctor

--- pig-dice-game.py
+++ pig-dice-game.py
@@ -1,10 +1,10 @@
 from random import randint
 class Pig_dice_game:
     def __init__(self):
-        pass
-    
+        self.TotalScore=0
+
     def new_game(self):
-        pass
+        pass    
 
     def roll_dice(self):
         pass
@@ -19,9 +19,6 @@ class Pig_dice_game:
 player1=Pig_dice_game()
 
 #게임 시작
-player1.new_gmae()
-player1.roll_dice()
-player1.bank()
-player1.turn()
+player1.new_game()
 
 #이후 컴퓨터 진행
