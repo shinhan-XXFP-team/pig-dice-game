e3afca0589d49b141ae5b924ce79f101edca4865
HaByeong <jhschris8080@naver.com>
2023-01-13 14:02:23 +0900

    feat: Create pig-dice-game class

---
+++ pig-dice-game.py
@@ -0,0 +1,27 @@
+from random import randint
+class Pig_dice_game:
+    def __init__(self):
+        pass
+    
+    def new_game(self):
+        pass
+
+    def roll_dice(self):
+        pass
+    
+    def turn(self):
+        pass
+    
+    def bank(self):
+        pass
+    
+
+player1=Pig_dice_game()
+
+#게임 시작
+player1.new_gmae()
+player1.roll_dice()
+player1.bank()
+player1.turn()
+
+#이후 컴퓨터 진행
